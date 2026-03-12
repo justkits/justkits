4ceea08bd50b5dd12544034507b0e364e826f201
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-12 23:45:36 +0900

    add test app

---
+++ apps/tester/tests/App.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+
+import { App } from "@/App";
+
+describe("App", () => {
+  it("should render", () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText("Tester App"));
+  });
+});
