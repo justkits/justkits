36c6eeb89382c41b6c29f5490165e9a54b55390f
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-18 14:58:14 +0900

    fix

--- apps/designs/src/main.tsx
+++ apps/designs/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { App } from "./App.tsx";
 
 import "@justkits/design-foundations/reset.css";
-import "@justkits/motion/keyframes.css";
 
 const root = document.getElementById("root")!; // NOSONAR
 
