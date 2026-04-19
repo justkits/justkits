2400654bdac81d634948dfe5174fef40a27be076
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-19 19:44:26 +0900

    exclude cli.ts from coverage collection

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     coverage: {
       exclude: [
         "**/index.ts",
+        "**/cli.ts",
         "**/*/tests/*",
         "**/dist/*",
         "**/node_modules/*",
