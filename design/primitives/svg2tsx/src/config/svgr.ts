3c85a446fee88e871658abde55f95c5123bdd083
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-04-23 12:59:22 +0900

    minor fix: explicit type import

--- design/primitives/svg2tsx/src/config/svgr.ts
+++ design/primitives/svg2tsx/src/config/svgr.ts
@@ -42,7 +42,7 @@ export const defaultSvgrConfig: SvgrConfig = {
   exportType: "named",
   template: (variables, { tpl }) => {
     return tpl`
-      import { IconProps } from "@justkits/svg2tsx";
+      import type { IconProps } from "@justkits/svg2tsx";
 
       export function ${variables.componentName}({ size = 16,...props }: Readonly<IconProps>) {
         return (${variables.jsx});
