f2ec9cf759dac293892632c471aa13dfe0d03f6a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-05-12 17:41:46 +0900

    export tab link

--- design/system/ui/src/atoms/Buttons/index.ts
+++ design/system/ui/src/atoms/Buttons/index.ts
@@ -1,3 +1,4 @@
 export { Button } from "./Button/Button";
 export { IconButton, type IconButtonProps } from "./IconButton/IconButton";
 export { Link } from "./Link/Link";
+export { TabLink, type TabLinkProps } from "./TabLink/TabLink";
