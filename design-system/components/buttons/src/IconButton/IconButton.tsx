efd31fbea934cc0d8942051b284f3827f33de6e3
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-05-31 19:38:31 +0900

    minor update: use children instead of icon

--- design-system/components/buttons/src/IconButton/IconButton.tsx
+++ design-system/components/buttons/src/IconButton/IconButton.tsx
@@ -3,33 +3,27 @@ import clsx from "clsx";
 import { Pressable, type PressableProps } from "@/Pressable";
 import { styles } from "../styles.css";
 
-export interface IconButtonProps extends Omit<
-  PressableProps,
-  "asChild" | "children"
-> {
-  icon: React.ReactNode;
-  label?: React.ReactNode;
+export interface IconButtonProps extends Omit<PressableProps, "children"> {
+  children: React.ReactNode;
   rounded?: boolean;
   ghost?: boolean;
   size?: "small" | "medium" | "large";
 }
 
 export function IconButton({
-  icon,
-  label,
+  children,
   rounded = false,
   ghost = false,
   size = "medium",
   className,
   ...rest
-}: IconButtonProps) {
+}: Readonly<IconButtonProps>) {
   return (
     <Pressable
       {...rest}
       className={clsx(styles.iconButton({ rounded, ghost, size }), className)}
     >
-      {icon}
-      {label}
+      {children}
     </Pressable>
   );
 }
