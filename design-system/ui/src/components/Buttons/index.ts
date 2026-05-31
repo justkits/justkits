fd0ff192b111c1cf31adb7fbfbca3f9f9e282736
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-05-31 19:51:19 +0900

    exports

---
+++ design-system/ui/src/components/Buttons/index.ts
@@ -0,0 +1,13 @@
+// buttons
+export { Pressable } from "@justkits/buttons/Pressable";
+export { IconButton } from "./IconButton/IconButton";
+
+export type { PressableProps } from "@justkits/buttons/Pressable";
+export type { IconButtonProps } from "./IconButton/IconButton";
+
+// links
+export { Link } from "@justkits/buttons/Link";
+export { TabLink } from "./TabLink/TabLink";
+
+export type { LinkProps } from "@justkits/buttons/Link";
+export type { TabLinkProps } from "./TabLink/TabLink";
