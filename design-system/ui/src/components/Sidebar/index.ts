7361dbd9295f70dfed4d6cadd175914f2ca3bfcc
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-09 18:52:27 +0900

    remove outdated components and simply re-export from sidebar package

--- design-system/ui/src/components/Sidebar/index.ts
+++ design-system/ui/src/components/Sidebar/index.ts
@@ -2,8 +2,9 @@ export { SidebarLink } from "./Items/Link";
 export { SidebarSectionLink } from "./Items/SectionLink";
 export { SidebarSection } from "./Items/Section";
 
-export { Sidebar } from "./Sidebar";
-export { SidebarNav } from "./Nav";
-export { SidebarToggle } from "./Toggle";
-
-export { SidebarProvider } from "@justkits/headless-ui/Sidebar";
+export {
+  SidebarProvider,
+  SidebarBody,
+  SidebarNav,
+  SidebarToggle,
+} from "@justkits/sidebar";
