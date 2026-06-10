3a2ad625a6f6c16ffae451742210dd250ab80096
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-10 16:32:50 +0900

    re-export in ui package

--- design-system/ui/src/components/Texts/index.ts
+++ design-system/ui/src/components/Texts/index.ts
@@ -1,1 +1,8 @@
+export { Keyboard, KeyboardGroup } from "@justkits/texts/Keyboard";
+export type {
+  KeyboardProps,
+  KeyboardGroupProps,
+  KeyboardOS,
+} from "@justkits/texts/Keyboard";
+
 export { Text } from "@justkits/texts/Text";
