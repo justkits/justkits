edc9b8e5ae0e3a11a93fe285c13aacfc7720ab11
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-13 22:41:35 +0900

    exports

---
+++ design-system/components/keyboard/src/index.ts
@@ -0,0 +1,4 @@
+export { useKeyboardShortkey } from "./hooks/useKeyboardShortkey";
+export type { UseKeyboardShortkeyOptions } from "./hooks/useKeyboardShortkey";
+
+export { formatShortcutAria, parseShortcutKeys } from "./utils";
