a79c9277fe457c37331629cdec61adaf65d80843
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-15 17:35:26 +0900

    update exports

--- design-system/components/keyboard/src/index.ts
+++ design-system/components/keyboard/src/index.ts
@@ -1,9 +1,12 @@
 export { useKeyboardShortkey } from "./hooks/useKeyboardShortkey";
-export type { UseKeyboardShortkeyOptions } from "./hooks/useKeyboardShortkey";
+export type {
+  UseKeyboardShortkeyOptions,
+  UseKeyboardShortkeyResult,
+} from "./hooks/useKeyboardShortkey";
 
 export { Keyboard } from "./components/Keyboard";
 export type { KeyboardProps } from "./components/Keyboard";
 export { KeyboardGroup } from "./components/KeyboardGroup";
 export type { KeyboardGroupProps } from "./components/KeyboardGroup";
 
-export type { OS } from "./utils/os";
+export type { Shortkey } from "./keys/types";
