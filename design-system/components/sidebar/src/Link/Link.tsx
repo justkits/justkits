effa445a3c1bbba5bfe87b795d556e0f503e86a1
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-19 15:49:52 +0900

    add SidebarLink component

---
+++ design-system/components/sidebar/src/Link/Link.tsx
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import { Link, type LinkProps } from "@justkits/headless-ui/Link";
+import clsx from "clsx";
+
+import { SidebarNavContext } from "@/contexts/internals";
+import { styles } from "./styles.css";
+
+export interface SidebarLinkProps extends Omit<LinkProps, "href" | "children"> {
+  children: React.ReactNode;
+  href: string;
+  isActive?: boolean;
+}
+
+export function SidebarLink({
+  children,
+  href,
+  isActive = false,
+  isDisabled = false,
+  isExternal = false,
+  className,
+  style,
+  ...rest
+}: Readonly<SidebarLinkProps>) {
+  const isInsideNav = useContext(SidebarNavContext);
+
+  if (!isInsideNav) {
+    throw new Error("Sidebar.Item must be used inside Sidebar.Nav.");
+  }
+
+  return (
+    <div
+      className={clsx(styles.link, className)}
+      style={style}
+      data-active={isActive || undefined}
+      data-disabled={isDisabled || undefined}
+    >
+      {children}
+      <Link
+        {...rest}
+        href={href}
+        isDisabled={isDisabled}
+        isExternal={isExternal}
+        aria-current={isActive ? "page" : undefined}
+        className={styles.linkOverlay}
+      />
+    </div>
+  );
+}
