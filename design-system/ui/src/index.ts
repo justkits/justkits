ac4fd20f01458a905fea32a280734baa183c1305
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-06-26 10:50:58 +0900

    re-export ThemeProvider, tokens and utility functions and import css files to generate an aggregate

---
+++ design-system/ui/src/index.ts
@@ -0,0 +1,10 @@
+import "@justkits/reset-css/reset.css";
+import "@justkits/theme/color-scheme.css";
+
+export { ThemeProvider, useTheme } from "@justkits/theme/light-dark";
+export {
+  justkitsDefault,
+  tokens,
+  mediaQueries,
+  colorWithOpacity,
+} from "@justkits/tokens";
